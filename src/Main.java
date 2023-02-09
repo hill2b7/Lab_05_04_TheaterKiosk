4e79c3715703755d19990154a38fc62f8952602f
blake <hill2b7@mail.uc.edu>
2023-02-08 19:34:26 -0500

    Project init

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        int age = 21; //declaration
+        final int LEGAL_AGE = 21; // declaration is a constant so that is why I used final int
+        if (age >= 21) //if statement that if the condition is true, the code block will run
+        {
+            System.out.println("You get a paper wrist band!"); //output statement
+        }
+
+    }
+}
